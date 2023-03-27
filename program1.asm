e961a66f4576c1090ad608a0315c69585d2fca21
djRemskii <jeremyong@cpp.edu>
2023-03-27 10:13:31 -0700

    Added Program 1 with initial information

---
+++ program1.asm
@@ -0,0 +1,25 @@
+#Name: Remi Ong
+#Date: 3/27/2023
+#Program: Program 1 
+#Goals:
+#   Task 1: User Input and Output
+#   In this task, you will practice getting user input, moving them to registers, and printing them back to the user. Below are the specific requirements for this task:
+#       A. Use "main: " label for this task
+#       B. Get 2 int values from the user
+#       C. The two user values should be held in registers $s0 and $s1, respectively
+#       D. Output the user inputs in the "Run I/O" section in MARS environment back to the user
+
+
+#   Task 2: Arithmetic Operation Practice
+#   Using the user input values in registers $s0 and $s1, write MIPS Assembly code to complete the arithmetic operations below:
+#       A. add two values (use instruction "add")
+#       B. subtract two values (use instruction "sub")
+#       C. multiply two values (use instruction "mul")
+#       D. divide two values (use instruction "div")
+#       E. Output the arithmetic results in the "Run I/O" section in MARS environment back to the user
+
+
+#   Task 3: Conditions
+#   In this final task, you will practice how to do conditions in Assembly:
+#       A. If the two user inputs from Task 1 are equal to each other, output "User inputs are the same"
+#       B. If the two user inputs from Task 1 are not equal to each other, output "User inputs are different"
